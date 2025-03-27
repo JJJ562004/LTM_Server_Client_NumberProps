c8f1613d0f8ed9c12dfa15e4be16137b184ea4fd
Nguyễn Ngọc Tuấn <143373366+JJJ562004@users.noreply.github.com>
2025-03-27 16:29:13 +0700

    Update ServerNumsProps.java

--- ServerNumsProps.java
+++ ServerNumsProps.java
@@ -177,7 +177,7 @@ public class ServerNumsProps {
                 DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 
                 try {
-                        String response = input.readUTF();
+                        String response = din.readUTF();
                         System.out.println("Nhận: " + message);
                     } catch (EOFException e) {
                         System.err.println("Client ngắt kết nối.");
